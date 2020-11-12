5cb963a6039ca22d9d84ae160a6d2faea3b3bac9
amahaya <babisko113@yahoo.fr>
2020-11-12 08:43:37 +0100

    get & display user

--- choirbook/choirbook/src/pages/index.js
+++ choirbook/choirbook/src/pages/index.js
@@ -6,8 +6,6 @@ import userS from "../services/UserService";
 const Home = () => {
 
   const [users, setUsers] = useState([]);
-  console.log("users");
-  console.log(users);
   const listUsers = users.map((user) =>
   <li key={user.id.toString()}>
     {user.firstName}
@@ -21,16 +19,11 @@ const Home = () => {
     };
     axios(config)
     .then(function (response) {
-      console.log("response",response,"response data", response.data);
-      console.log(JSON.stringify(response.data));
       setUsers(response.data);
     })
     .catch(function (error) {
-      console.log(error);
     });
   },[])
   return (listUsers)
-
 }
-
 export default Home;
